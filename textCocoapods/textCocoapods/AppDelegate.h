8d934fbb381815b4ed0731944df0e276d28bc9a5
zhangxi <zhangxi_1989@sina.com>
2013-08-21 10:38:18 +0800

    init

---
+++ textCocoapods/textCocoapods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  textCocoapods
+//
+//  Created by zhangxi on 13-8-21.
+//  Copyright (c) 2013年 zhangxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ textCocoapods/textCocoapods/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  textCocoapods
+//
+//  Created by zhangxi on 13-8-21.
+//  Copyright (c) 2013年 zhangxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
